f21f8da7cadaf36272a4fed70345008d3e99a422
kinakome <ryomatsu0120@gmail.com>
2020-10-29 19:29:41 +0900

    [Add] Firebase + CircleCI settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: ['~/plugins/firebase'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -37,6 +37,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
